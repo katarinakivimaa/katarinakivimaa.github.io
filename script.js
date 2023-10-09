0eb7771a35ef7125afcb4cf010bc096515d8bd5c
katarinakivimaa <112554461+katarinakivimaa@users.noreply.github.com>
2023-10-09 06:42:43 +0300

    natuke progressi

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-};
+gsap.registerPlugin(Flip);
+
+const links = document.querySelectorAll(".nav-item a");
+const activeNav = document.querySelector(".active-nav");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+    //nav to blue
+    gsap.to(links, {color:"#F8F8F8"});
+    if (document.activeElement === link){
+    gsap.to(link, {color:"#000000"});
+    }
+    
+    //i like to move it move it
+    const state = Flip.getState(activeNav);
+    link.appendChild(activeNav);
+    Flip.from(state, {
+        duration: 0.5,
+        absolute: true,
+    });
+    
+});
+});
+
+
+
+
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// };
+
+
