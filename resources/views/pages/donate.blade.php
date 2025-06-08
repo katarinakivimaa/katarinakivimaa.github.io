c14e5ea03b0ea4dcc92a05090a5986d5151756f7
katarinakivimaa <112554461+katarinakivimaa@users.noreply.github.com>
2025-06-08 21:42:29 +0300

    Donate page style update

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -1,10 +1,11 @@
 @extends('partials.layout')
 @section('content')
-<div class="min-h-screen pt-32 flex justify-center items-start font-primary px-8">
-  <div class="flex max-w-6xl w-full gap-16">
+<div style="background-image: url('/windmill_banner.jpg');" class="w-98 absolute top-auto w-full h-screen relative font-primary">
+<div class="pt-24 flex justify-center items-start font-primary px-8">
+  <div class="flex max-w-6xl w-full items-center h-screen gap-16">
 
-    <div class="flex-shrink-0">
-      <img src="/windmill_banner" class="max-w-xs" />
+    <div class="">
+      <img src="/windmill_banner.jpg" class="" />
     </div>
 
     <div class="flex-1 p-8 bg-white rounded-3xl shadow-lg">
