6c897a1f6c975829c1609f9fa0b25a87f10b79d8
katarinakivimaa <112554461+katarinakivimaa@users.noreply.github.com>
2025-06-08 17:46:19 +0300

    Code ongoing in index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,16 +1,18 @@
 @extends('partials.layout')
 @section('content')
+
 <div class="w-full h-screen">
-    <div class="absolute top-auto w-full h-screen bg-primary"></div>
+    <div style="background-image: url('/kiiu_hero_banner.jpg');" class="absolute top-0 bg-cover bg-center bg-[url('/kiiu_hero_banner.jpg')] w-full h-screen"></div>
     <div class="relative z-10 text-white pt-96 ml-24">
         <div class="text-3xl font-primary">
-            <p class="mb-6">Come and discover
+             
+            <p class="mb-2">Come and discover
                 <span class="uppercase text-primary">Kiiu Tuulik</span>
             </p>
             <p class="font-primary text-6xl capitalize">Discover history</p>
-            <p class="font-primary text-6xl capitalize mb-12">
+            <p class="font-primary text-6xl capitalize mb-6">
                 Through
-                <span class="text-white">Wonderful Discovery</span>.
+                <span class="text-primary">Wonderful Discovery</span>.
             </p>
         </div>
 
@@ -27,8 +29,36 @@
     </div>
 </div>
 
+<div class="w-full bg-primary flex justify-center items-center py-10">
+    <div class="flex justify-center items-center gap-16 max-w-7xl px-6">
+        <div class="flex flex-col justify-center items-center gap-8 max-w-xl text-center">
+            <div class="text-white text-8xl font-normal font-['KoHo']">
+                History
+            </div>
+            <div class="flex space-y-4 flex-col gap-10 max-w-xl">
+                <div>
+                    <h2 class="text-white text-3xl font-bold font-['KoHo'] mb-2">Grant Wood</h2>
+                    <p class="text-white text-base font-normal font-['KoHo'] leading-relaxed">
+                        Grant Wood, famous for his representations of the Midwest—of which the Art Institute’s American Gothic (1930.934) is his most recognizable example—intended The Pump to be an illustration for Sinclair Lewis’s novel Main Street (1920).
+                    </p>
+                </div>
+                <div>
+                    <h2 class="text-white text-3xl font-bold font-['KoHo'] mb-2">American Gothic</h2>
+                    <p class="text-white text-base font-normal font-['KoHo'] leading-relaxed">
+                        This image of a contemporary water pump would have been a perfect fit for Lewis’s novel about life in a midwestern town. The drawing was given to the Art Institute by Carter Manny, Jr., who received it as a high school graduation gift from the artist in 1937.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <img
+            class="w-96 rounded-t-[200px]" style="border-top-left-radius: 200px; border-top-right-radius: 200px"
+            src="/kiiu_pilt.jpg" />
+    </div>
+</div>
+
+
 <div class="w-full">
-    <div class="absolute w-full top-auto my-16"></div>
+    <div class="absolute w-full top-8 my-16"></div>
     <div class="relative z-10 mx-32 text-white flex items-center gap-6">
         <h3 class="text-black text-xl">A flash to the past</h3>
         <h1 class="text-6xl text-primary">Support a historical monument</h1>
@@ -43,7 +73,7 @@
                 <div class="w-64 justify-center text-black text-m">This evocative character study is an early example of a type of subject that preoccupied the great Dutch master Rembrandt van Rijn throughout his long career. Although his large output included landscapes, genre paintings, and the occasional still.</div>
             </div>
             <div class="inline-flex w-12 flex-col justify-start items-start gap-3.5">
-                <img style="width: auto; height: 360px; border-top-left-radius: 160px; border-top-right-radius: 160px"" src=" /pats.jpg" />
+                <img style="width: auto; height: 360px; border-top-left-radius: 160px; border-top-right-radius: 160px" src="/pats.jpg" />
                 <div class="self-stretch justify-start items-start gap-14">
                     <div class="w-7 h-5 justify-center text-black text-xl font-medium ">1631</div>
                     <div class="w-52 h-5 text-right justify-center text-black text-m ">Rembrandt Harmensz. van Rijn</div> <br>
@@ -52,7 +82,7 @@
                 <div class="w-64 justify-center text-black text-m">This evocative character study is an early example of a type of subject that preoccupied the great Dutch master Rembrandt van Rijn throughout his long career. Although his large output included landscapes, genre paintings, and the occasional still.</div>
             </div>
             <div class="inline-flex w-12 flex-col justify-start items-start gap-3.5">
-                <img style="width: auto; height: 360px; border-top-left-radius: 160px; border-bottom-right-radius: 160px"" src=" /download" />
+                <img style="width: auto; height: 360px; border-top-left-radius: 160px; border-bottom-right-radius: 160px" src="/download" />
                 <div class="self-stretch justify-start items-start gap-14">
                     <div class="w-7 h-5 justify-center text-black text-xl font-medium ">1631</div>
                     <div class="w-52 h-5 text-right justify-center text-black text-m ">Rembrandt Harmensz. van Rijn</div> <br>
@@ -66,30 +96,22 @@
 </div>
 </div>
 
-
-<div class="w-full justify-center items-center">
-    <div class="w-96 h-36 relative">
-        <div class="w-72 h-6 left-0 top-0 absolute justify-center text-black text-3xl font-bold font-['Futura']">American Gothic</div>
-        <div class="w-96 h-20 left-0 top-[61px] absolute justify-center text-black text-xs font-medium font-['Futura']">This image of a contemporary water pump would have been a perfect fit for Lewis’s novel about life in a midwestern town. The drawing was given to the Art Institute by Carter Manny, Jr., who received it as a high school graduation gift from the artist in 1937.</div>
-    </div>
-    <div class="w-96 h-36 relative">
-        <div class="w-52 h-6 left-0 top-0 absolute justify-center text-black text-3xl font-bold font-['Futura']">Grant Wood</div>
-        <div class="w-96 h-20 left-0 top-[61px] absolute justify-center text-black text-xs font-medium font-['Futura']">Grant Wood, famous for his representations of the Midwest—of which the Art Institute’s American Gothic (1930.934) is his most recognizable example—intended The Pump to be an illustration for Sinclair Lewis’s novel Main Street (1920). </div>
-    </div>
-    <div class="w-[662px] h-44 relative">
-        <div class="left-0 top-0 absolute justify-center text-black text-9xl font-normal font-['KoHo'] [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)]">THE AQ ART</div>
-    </div>
-
-</div>
-
-
 <div class="w-full h-fit justify-center items-center">
     <div class="relative space-y-4 z-10 h-full pt-32 py-16 mx-32 text-white text-center font-primary">
-        <h3 class="text-black text-2xl">A flash to the past</h3>
+        <h3 class="text-black text-2xl">A flash to the past</h3> <br>
         <h1 class="text-6xl text-primary">Support a historical monument</h1>
+        <div class="" style="transform:translateY(280px)"><button style="transform:translateX(-845px)" onclick="scrollSlider(-1)"
+                class="absolute left-0 top-1/3 bg-black text-2xl text-bold text-primary p-4 rounded-full shadow-lg hover:bg-primary hover:text-white transition">
+                ‹
+            </button>
+            <button style="transform:translateX(20px)" onclick="scrollSlider(1)"
+                class="absolute right-0 top-1/3 bg-black text-2xl text-bold text-primary p-4 rounded-full shadow-lg hover:bg-primary hover:text-white transition">
+                ›
+            </button>
+        </div>
         <div class="grid grid-cols-4 gap-16">
             @foreach($products as $product)
-            <div class="bg-primary rounded-xl overflow-hidden">
+            <div class="bg-primary rounded-xl mt-8 overflow-hidden">
                 <div class="h-96">
                     <img class="object-cover h-inherit w-inherit"
                         src="{{ isset($product->image[0]) ? Storage::disk('public')->url($product->image[0]) : asset('images/default.png') }}">
@@ -102,6 +124,15 @@
             </div>
             @endforeach
         </div>
+
+        <div class="flex justify-center mt-6 gap-4">
+            <div class="w-2 h-2 bg-primary rounded-full"></div>
+            <div class="w-2 h-2 bg-black rounded-full"></div>
+            <div class="w-2 h-2 bg-black rounded-full"></div>
+            <div class="w-2 h-2 bg-black rounded-full"></div>
+        </div>
+
+
     </div>
 </div>
 @endsection
